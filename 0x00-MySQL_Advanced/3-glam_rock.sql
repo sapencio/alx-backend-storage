ce799790cec469eea84f8c321a28310c721925b2
Tibebe Solomon <62059876+sapencio@users.noreply.github.com>
2023-01-12 14:19:21 +0300

    Old school band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,4 @@
 -- Lists all bands with Glam rock as their main style, ranked by their longevity
 -- SELECT band_name, (IFNULL(split, YEAR(CURRENT_DATE())) - formed) AS lifespan
 SELECT band_name, COALESCE(split, 2020) - formed as lifespan
-FROM metal_bands WHERE style LIKE '%Glam rock%' 
-ORDER BY lifespan DESC;
+FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
